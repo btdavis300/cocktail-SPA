20a3e3a934de6fd5222f54b282289eae229f4a62
Brennan Davis <brennantd@gmail.com>
2022-07-11 22:03:14 -0600

    added button functionality and css styles

--- index.js
+++ index.js
@@ -28,17 +28,47 @@
 //Global Variables
 const baseURL = ("http://localhost:3000/drinks")
 let page = 1
-let cocktailImageContainer = document.querySelector("#cocktail-image-container")
+let forwardButton = document.getElementById("forward")
+let backButton = document.getElementById('backward')
+
+//Event Listeners
+forwardButton.addEventListener('click', forwardPage)
+backButton.addEventListener('click', backwardPage)
 
 //Render Image Functions
 function renderImages(drinksArr) {
+    const cocktailImageContainer = document.querySelector("#cocktail-image-container")
+    if (cocktailImageContainer.children.length > 0) {
+        const cocktailClass = document.querySelectorAll(".imageItem")
+        cocktailClass.forEach(drink => {
+            drink.remove()
+        })
+    }
+
     drinksArr.forEach(drink => {
-        let cocktailThumbnail = document.createElement('img')
+        const cocktailThumbnail = document.createElement('img')
         cocktailThumbnail.src = drink.strDrinkThumb
-        console.log(cocktailThumbnail)
+        cocktailThumbnail.classList.add("imageItem")
         cocktailImageContainer.appendChild(cocktailThumbnail)
     });
 
+}
+
+//Button Functions
+function forwardPage() {
+    if (page < 10) {
+        page += 1
+        fetchTwentyDrinks(page)
+    }
+}
+
+function backwardPage() {
+    if (page > 1) {
+        page -= 1
+        fetchTwentyDrinks(page)
+    } else if (page = 1) {
+        alert("You've gone too far. TURN BACK!")
+    }
 }
 
 //Fetch Functions
@@ -50,7 +80,7 @@ function fetchAllDrinks() {
         })
 }
 
-function fetchTwentyDrinks() {
+function fetchTwentyDrinks(page = 1) {
     fetch(baseURL + `/?_limit=20&_page=${page}`)
         .then(r => r.json())
         .then(drinksArr => renderImages(drinksArr))
